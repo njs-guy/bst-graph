368d8ba60fba3c04758d0018dd7594674f22c341
Nick Shockey <shockeynj@gmail.com>
2022-06-26 13:05:18 -0400

    Set up tailwind css

--- src/App.vue
+++ src/App.vue
@@ -17,11 +17,11 @@ export default defineComponent({
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: black;
   margin-top: 60px;
 }
 </style>
