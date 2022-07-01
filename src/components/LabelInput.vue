025a04dcb06a6d26d17aa386fe36b7b5bf1a639f
Nick Shockey <shockeynj@gmail.com>
2022-07-01 11:34:31 -0400

    LabelInputs now have correct id's

--- src/components/LabelInput.vue
+++ src/components/LabelInput.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="input-container flex flex-col">
-        <label for="{{idName}}" class="place-self-start font-bold">{{text}}</label>
-        <input type="number" class="rounded outline-indigo-700 caret-indigo-500 outline-offset-0 h-8 px-2" id="{{idName}}" name="{{idName}}">
+        <label :for="idName" class="place-self-start font-bold">{{text}}</label>
+        <input type="number" class="rounded outline-indigo-700 caret-indigo-500 outline-offset-0 h-8 px-2" :id="idName" :name="idName">
     </div>
 </template>
 
