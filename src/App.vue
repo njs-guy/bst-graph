0ca888bf8ac155947e553776c0156d26b2f256f6
Nick Shockey <shockeynj@gmail.com>
2022-07-24 13:36:50 -0400

    Autofill now works with all pokemon by default
    probably

--- src/App.vue
+++ src/App.vue
@@ -129,7 +129,7 @@ export default defineComponent({
     },
     async fetchStats(pokName:string = "bidoof") {
       let statArr:any = [];
-      let name = pokName.toLowerCase();
+      let name = this.checkForForms(pokName.toLowerCase());
 
       const api = new PokemonClient({
         cacheOptions: { maxAge: 5000 },
@@ -151,9 +151,72 @@ export default defineComponent({
     checkForForms(pokName:string)
     {
       switch (pokName) {
+        // No response
         case "deoxys":
-          return "deoxys";
+          return "deoxys-normal";
           break;
+        case "wormadam":
+          return "wormadam-plant";
+          break;
+        case "giratina":
+          return "giratina-altered";
+          break;
+        case "shaymin":
+          return "shaymin-land";
+          break;
+        case "basculin":
+          return "basculin-red-striped";
+          break;
+        case "darmanitan":
+          return "darmanitan-standard";
+          break;
+        case "tornadus":
+          return "tornadus-incarnate";
+          break;
+        case "thundurus":
+          return "thundurus-incarnate";
+          break;
+        case "landorus":
+          return "landorus-incarnate";
+          break;
+        case "keldeo":
+          return "keldeo-ordinary";
+          break;
+        case "meloetta":
+          return "meloetta-aria";
+          break;
+        case "aegislash":
+          return "aegislash-shield";
+          break;
+        case "pumpkaboo":
+          return "pumpkaboo-average";
+          break;
+        case "gourgeist":
+          return "gourgeist-average";
+          break;
+        case "zygarde":
+          return "zygarde-50";
+          break;
+        case "oricorio":
+          return "oricorio-baile";
+          break;
+        case "lycanroc":
+          return "lycanroc-midday";
+          break;
+        case "wishiwashi":
+          return "wishiwashi-school";
+          break;
+        case "minior":
+          return "minior-red-meteor";
+          break;
+        case "eiscue":
+          return "eiscue-ice";
+          break;
+        case "morpeko":
+          return "morpeko-hangry";
+          break;
+        default:
+          return pokName;
       }
     },
     fillGraph(stats:Array<number>)
