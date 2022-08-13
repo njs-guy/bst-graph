3286db1277ce76a0186799dfdc56b19384098cf8
Nick Shockey <shockeynj@gmail.com>
2022-08-13 14:26:59 -0400

    Added basic theme switcher
    Doesn't do anything yet

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,18 @@
 <template>
     <div class="header flex flex-row p-2 bg-panel dark:bg-panel-dark">
-        <h1 class="text-2xl">BST Graph</h1>
+        <h1 class="text-2xl mr-auto">BST Graph</h1>
+        <ThemeSwitcher class="float-right" />
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import ThemeSwitcher from './ThemeSwitcher.vue';
 
 export default defineComponent({
     Name: "Header",
-    methods: {
-
-    },
+    methods: {},
+    components: { ThemeSwitcher }
 });
 </script>
 
---
+++ src/components/ThemeSwitcher.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="theme-switcher self-center">
+        <input type="checkbox" class="mr-1" id="theme" name="theme" value="dark" />
+        <label for="theme" class="text-lg">Dark mode</label>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    Name: "ThemeSwitcher",
+    methods: {
+
+    },
+});
+</script>
+
+<style scoped>
+
+</style>
