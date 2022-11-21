cf96592cc720f24cc2f7468e2712dae5d7795676
Nick Shockey <shockeynj@gmail.com>
2022-11-21 15:24:01 -0500

    Fixed label-to-bar ratio
    This means that high numbers won't mess up the layout
    Especially with defense (stupid shuckle)

--- src/components/StatBar.vue
+++ src/components/StatBar.vue
@@ -1,10 +1,10 @@
 <template>
 	<div class="row flex flex-row items-center gap-2">
-		<div class="text-container basis-1/3">
+		<div class="text-container basis-5/12">
 			<p class="label self-center">{{ label }}:</p>
 			<p class="stat">{{ stat }}</p>
 		</div>
-		<div class="bar-container basis-2/3">
+		<div class="bar-container basis-7/12">
 			<div
 				class="bar"
 				:class="barId"
@@ -90,11 +90,13 @@ export default defineComponent({
 }
 
 @media (min-width: 32rem) {
+	/* Change the label-to-bar ratio when on desktop */
+
 	.text-container {
-		@apply basis-1/4;
+		@apply basis-1/3;
 	}
 	.bar-container {
-		@apply basis-3/4;
+		@apply basis-2/3;
 	}
 }
 </style>
