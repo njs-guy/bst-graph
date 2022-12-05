5f9312ec3ad814ca94f9057b92c6151d402b851b
Nick Shockey <shockeynj@gmail.com>
2022-12-04 19:37:07 -0500

    Dark mode switcher works again

--- src/modules/appConfig.ts
+++ src/modules/appConfig.ts
@@ -9,12 +9,13 @@ export function saveTheme(dark: string) {
 }
 
 export function changeTheme(darkMode: boolean) {
-	const html = document.getElementsByTagName("html")[0];
+	const html = document.documentElement;
+	html.removeAttribute("data-theme"); // remove previous theme
 
 	if (darkMode) {
-		html.classList.add("dark");
+		html.setAttribute("data-theme", "dark");
 	} else {
-		html.classList.remove("dark");
+		html.setAttribute("data-theme", "light");
 	}
 
 	saveTheme(String(darkMode));
@@ -23,18 +24,26 @@ export function changeTheme(darkMode: boolean) {
 export function loadSettings() {
 	const quality = localStorage.getItem("quality");
 	const dark = localStorage.getItem("dark");
+	let darkBool = false;
 
 	const qSel = document.getElementById("quality-select") as HTMLSelectElement;
 	const themeSw = document.getElementById("theme") as HTMLInputElement;
 
+	// ------Quality------
 	// Check if there's a saved setting for quality
 	if (quality !== null) {
 		qSel.value = quality; // Set the value of the quality selector
 	}
 
+	// ------Dark------
 	// Check if dark mode is true
+	// LocalStorage can only store strings, so booleans have to be converted first.
 	if (dark === "true") {
-		changeTheme(true); // Change theme
-		themeSw.checked = true; // Check theme checkbox
+		darkBool = true;
+	} else {
+		darkBool = false;
 	}
+
+	changeTheme(darkBool); // Change theme
+	themeSw.checked = darkBool; // Check theme checkbox
 }
