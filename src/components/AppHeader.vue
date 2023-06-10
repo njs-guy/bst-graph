1b0fa591e37654aa0ecae6d30d1243016d779b5b
njshockey <shockeynj@gmail.com>
2023-06-09 20:27:43 -0400

    Added logo to header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="navbar bg-base-200 rounded-md mb-4">
-		<h1 class="text-2xl ml-2 mr-auto">BST Graph</h1>
+		<h1 class="text-2xl ml-2 mr-auto"><LogoIcon />BST Graph</h1>
 		<GitHubIcon />
 		<ThemeSwitcher
 			@darkMode="onDarkMode"
@@ -11,8 +11,9 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import ThemeSwitcher from "./ThemeSwitcher.vue";
 import GitHubIcon from "./GitHubIcon.vue";
+import LogoIcon from "./LogoIcon.vue";
+import ThemeSwitcher from "./ThemeSwitcher.vue";
 
 export default defineComponent({
 	Name: "AppHeader",
@@ -24,6 +25,7 @@ export default defineComponent({
 	components: {
 		ThemeSwitcher,
 		GitHubIcon,
+		LogoIcon,
 	},
 });
 </script>
---
+++ src/components/LogoIcon.vue
@@ -0,0 +1,102 @@
+<template>
+	<svg
+		viewBox="0 0 26.950274 26.950274"
+		class="w-12 h-12 mr-2"
+		alt="BST Graph logo"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<title>BST Graph</title>
+		<g
+			id="layer1"
+			transform="translate(-9.6616352,-6.324601)"
+		>
+			<rect
+				style="fill: #fafafa"
+				width="26.696274"
+				height="26.696274"
+				x="9.7886353"
+				y="6.451601"
+				ry="1.5"
+			/>
+			<text
+				x="12.02909"
+				y="14.345181"
+			>
+				<tspan
+					style="
+						font-style: normal;
+						font-variant: normal;
+						font-weight: bold;
+						font-stretch: normal;
+						font-size: 8.46667px;
+						font-family: Cabin;
+					"
+					x="12.02909"
+					y="14.345181"
+				>
+					B
+				</tspan>
+				<tspan
+					style="
+						font-style: normal;
+						font-variant: normal;
+						font-weight: bold;
+						font-stretch: normal;
+						font-size: 8.46667px;
+						font-family: Cabin;
+					"
+					x="12.02909"
+					y="22.763073"
+				>
+					S
+				</tspan>
+				<tspan
+					style="
+						font-style: normal;
+						font-variant: normal;
+						font-weight: bold;
+						font-stretch: normal;
+						font-size: 8.46667px;
+						font-family: Cabin;
+					"
+					x="12.02909"
+					y="31.180964"
+				>
+					T
+				</tspan>
+			</text>
+			<rect
+				style="fill: #ef4444"
+				width="9.0099945"
+				height="5.4504895"
+				x="18.353691"
+				y="8.3982029"
+				ry="1"
+			/>
+			<rect
+				style="fill: #3b82f6"
+				width="5.1167874"
+				height="5.4504895"
+				x="18.353691"
+				y="17.074493"
+				ry="1"
+			/>
+			<rect
+				style="fill: #22c55e"
+				width="15.684064"
+				height="5.4504895"
+				x="18.353689"
+				y="25.750784"
+				ry="1"
+			/>
+		</g>
+	</svg>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	name: "LogoIcon",
+});
+</script>
