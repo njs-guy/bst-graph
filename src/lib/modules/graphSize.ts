849e4640f255a76efbd657b84614c3f5b65674df
njs-guy <shockeynj@gmail.com>
2025-11-18 20:19:29 -0500

    placeholder graph sizes that don't do anything yet

---
+++ src/lib/modules/graphSize.ts
@@ -0,0 +1,33 @@
+export interface GraphSize {
+	width: number;
+	rowHeight: number;
+	barHeight: number;
+	fontSize: number;
+	nameFontSize: number;
+}
+
+// Placeholder graph sizes
+
+export const smallGraph: GraphSize = {
+	width: 5,
+	rowHeight: 5,
+	barHeight: 5,
+	fontSize: 16,
+	nameFontSize: 24,
+};
+
+export const baseGraph: GraphSize = {
+	width: 5,
+	rowHeight: 5,
+	barHeight: 5,
+	fontSize: 16,
+	nameFontSize: 24,
+};
+
+export const largeGraph: GraphSize = {
+	width: 5,
+	rowHeight: 5,
+	barHeight: 5,
+	fontSize: 16,
+	nameFontSize: 24,
+};
